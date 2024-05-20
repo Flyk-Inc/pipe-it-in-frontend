f378d1ef061db003e229adae1a3caf97014a3c57
lyeslimani <lyes.slimani.dev@gmail.com>
2024-05-20 20:08:02 +0200

    fix: lint check

--- src/app/component/layout/button/button.component.ts
+++ src/app/component/layout/button/button.component.ts
@@ -1,19 +1,14 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
-  standalone: true,
-  imports: [],
-  templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+	selector: 'app-button',
+	standalone: true,
+	imports: [],
+	templateUrl: './button.component.html',
+	styleUrl: './button.component.css',
 })
 export class ButtonComponent {
-  @Input() text!: string;
-  @Input() disabled!: boolean;
-  @Output() onClick = new EventEmitter<any>();
-  @Input() type?: 'submit' | 'reset' | 'button' | undefined;
-
-  onClickButton(event: MouseEvent) {
-    this.onClick.emit(event);
-  }
+	@Input() text!: string;
+	@Input() disabled!: boolean;
+	@Input() type?: 'submit' | 'reset' | 'button' | undefined;
 }
