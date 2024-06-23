859d82c4dcf6ab0e9c01e5ae812d762283b62a5c
flammefolie <fguillemont@myges.fr>
2024-06-23 21:19:51 +0200

    feat: popular groups to join in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from '../../component/nav/sidenav/sidenav.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { PopularGroupsComponent } from './popular-groups/popular-groups.component';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [SidenavComponent, TimelineComponent],
+	imports: [SidenavComponent, TimelineComponent, PopularGroupsComponent],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss',
 })
